2e22b6bf433b4057f7ccbadc55e710bd27432177
Алексей Яганов <btls3@yandex.ru>
2020-04-27 16:54:43 +0300

    After small changes

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -15,64 +15,69 @@ public class Calculator {
      */
 
     private static String operators = "+-*/";
-    private static String delimiters = "() ";
+    private static String separators = "() ";
     public static boolean flag = true;
     private static Map<String, Integer> priorityMap;
-    private static Set<String> delimiterSet;
+    private static Set<String> separatorSet;
     private static Set<String> operatorSet;
+    // создание таблиц операторов, разделителей и приоритетов операций
     static {
         priorityMap = new HashMap<>();
-        delimiterSet = new HashSet<>();
+        separatorSet = new HashSet<>();
         operatorSet = new HashSet<>();
         priorityMap.put("(", 1);
         priorityMap.put("-", 2);
         priorityMap.put("+", 2);
         priorityMap.put("*", 3);
         priorityMap.put("/", 3);
-        for (int i = 0; i < delimiters.length(); i++) {
-            delimiterSet.add("" + delimiters.charAt(i));
+        for (int i = 0; i < separators.length(); i++) {
+            separatorSet.add("" + separators.charAt(i));
         }
         for (int i = 0; i < operators.length(); i++) {
             operatorSet.add("" + operators.charAt(i));
         }
     }
 
-    private static boolean isDelimiter(String token) {
-        return delimiterSet.contains(token);
+    // проверка текущего символа на разделители
+    private static boolean isDelimiter(String sign) {
+        return separatorSet.contains(sign);
     }
-
+    // проверка текущего символа на разделители
     private static boolean isOperator(String token) {
         return operatorSet.contains(token);
     }
 
-    private static int priority(String token) throws CalculatorParseException{
-        int priority = priorityMap.get(token);
-        if (token == null)
-            throw new CalculatorParseException("wrong priority item");
+    // задание приоритетов операций
+    private static int priority(String sign) throws CalculatorParseException{
+        int priority = priorityMap.get(sign);
+        if (sign == null)
+            throw new CalculatorParseException("Не возможно задать приоритет этому оператору");
         return priority;
     }
 
-    private static TokenType typeRecognizer(String token) {
-        if (token.equals(" "))
-            return TokenType.SPACE;
-        if (isOperator(token))
-            return TokenType.OPERATOR;
-        if (isDelimiter(token))
-            return TokenType.DELIMITER;
-        return TokenType.OPERAND;
+    // Отнесение каждого входного char к типу разделителей, операторов, пробелов или цифр
+    private static SignType typeRecognizer(String sign) {
+        if (sign.equals(" "))
+            return SignType.SPACE;
+        if (isOperator(sign))
+            return SignType.OPERATOR;
+        if (isDelimiter(sign))
+            return SignType.SEPARATOR;
+        return SignType.NUMERAL;
     }
 
-    private enum TokenType{
+    private enum SignType{
         SPACE,
-        OPERAND,
+        NUMERAL,
         OPERATOR,
-        DELIMITER
+        SEPARATOR
     }
 
-    public static List<String> parse(String infix) throws CalculatorParseException {
-        List<String> strOut = new ArrayList<String>();
-        Deque<String> stack = new ArrayDeque<String>();
-        StringTokenizer tokenizer = new StringTokenizer(infix, delimiters + operators, true);
+    // функция парсинга входящей строки
+    public static List<String> parse(String input) throws CalculatorParseException {
+        List<String> output = new ArrayList<String>();
+        Deque<String> stack = new ArrayDeque<String>(); // очередь(стек)
+        StringTokenizer tokenizer = new StringTokenizer(input, separators + operators, true); //анализатор текста, где операторы и скобки являются разделителями
         String prev = "";
         String curr = "";
         while (tokenizer.hasMoreTokens()) {
@@ -81,17 +86,19 @@ public class Calculator {
                 case SPACE:
                     continue;
 
-                case OPERAND:
-                    strOut.add(curr);
+                case NUMERAL:
+                    if (curr.equals(".")&& prev.equals("."))
+                        throw new CalculatorParseException("Некорректное использование разделителя в десятичных чисел");
+                    output.add(curr);
                     break;
 
-                case DELIMITER:
+                case SEPARATOR:
                     if (curr.equals("(")) stack.push(curr);
                     else if (curr.equals(")")) {
                         while (!stack.peek().equals("(")) {
-                            strOut.add(stack.pop());
+                            output.add(stack.pop());
                             if (stack.isEmpty()) {
-                                throw new CalculatorParseException("Parentheses unpaired");
+                                throw new CalculatorParseException("Нету парной закрывающейся скобки");
                             }
                         }
                         stack.pop();
@@ -100,14 +107,14 @@ public class Calculator {
 
                 case OPERATOR:
                     if (!tokenizer.hasMoreTokens()) {
-                        throw new CalculatorParseException("Incorrect syntax");
+                        throw new CalculatorParseException("Некорректный синтаксис");
                     }
                     if (curr.equals("-") && (prev.equals("") || (isDelimiter(prev)  && !prev.equals(")")))) {
-                        strOut.add("0");
+                        output.add("0");
                     }
                     else {
                         while (!stack.isEmpty() && (priority(curr) <= priority(stack.peek()))) {
-                            strOut.add(stack.pop());
+                            output.add(stack.pop());
                         }
 
                     }
@@ -115,21 +122,22 @@ public class Calculator {
                     break;
             }
             if (isOperator(prev) && isOperator(curr))
-                throw new CalculatorParseException("two operands in a row operand");
+                throw new CalculatorParseException("два оператора идут друг за другом");
             prev = curr;
         }
 
         while (!stack.isEmpty()) {
-            if (isOperator(stack.peek())) strOut.add(stack.pop());
+            if (isOperator(stack.peek())) output.add(stack.pop());
             else {
-                throw new CalculatorParseException("Parentheses unpaired");
+                throw new CalculatorParseException("Нету парной закрывающейся скобки");
             }
         }
-        return strOut;
+        return output;
     }
 
+    // достаем значения из стека и производим арифметические операции
     public static Double calc(List<String> strOut) throws CalculateException {
-        Deque<Double> stack = new ArrayDeque<Double>();
+        Deque<Double> stack = new ArrayDeque<Double>();  // очередь(стек)
         Double a, b;
         for (String x : strOut) {
             switch (x) {
@@ -150,7 +158,7 @@ public class Calculator {
                 case "/":
                     b = stack.pop();
                     if (b == 0)
-                        throw new CalculateException("division by null");
+                        throw new CalculateException("деление на ноль");
                     a = stack.pop();
                     stack.push(a / b);
                     break;
@@ -160,19 +168,23 @@ public class Calculator {
                         stack.push(Double.parseDouble(x));
                         break;
                     } catch (NumberFormatException e) {
-                        throw new CalculateException("Element parse error");
+                        throw new CalculateException("Ошибка парсинга элемента");
                     }
             }
         }
         return stack.pop();
     }
 
+        // оценка и проверка результата калькуляции
     public String evaluate(String statement) {
         if (statement == null || statement == "")
             return null;
         String stringResult;
         try {
             Double doubleResult = calc(parse(statement));
+            doubleResult*=10000;
+            int intres = (int) Math.round(doubleResult);
+            doubleResult=(double)intres/10000;
             if (doubleResult == Math.floor(doubleResult)) {
                 stringResult = String.valueOf(doubleResult.intValue());
             } else {
--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -22,12 +22,12 @@ public class Subsequence {
             if (y.isEmpty()) return false;
 
             int index = 0;
-            boolean check = false;
+            boolean result = false;
             //проходим по елементам первого списка
             for (int i = 0; i < x.size(); i++) {
                 //если данный элемент не содержится во втором списке, то прекратить проверку с отрицательным результатом
                 if (!y.contains(x.get(i))) {
-                    check = false;
+                    result = false;
                     break;
                 } else {
                     //в переменную index сохраняем индекс предыдущего элемента второго списка
@@ -37,21 +37,21 @@ public class Subsequence {
 
                         if (x.get(i) != y.get(j)) {
                             j++;
-                            if (check) check = false;
+                            if (result) result = false;
                         } else {
-                            check = true;
+                            result = true;
                             index = j;
                             break;
                         }
                     }
                     //если второй список кончился раньше, чем перебрали все элементы первого вернуть False
                     if ((j == y.size()) && (i != x.size())) {
-                        check = false;
+                        result = false;
                         break;
                     }
                 }
             }
-            return check;
+            return result;
 
         }
     }
